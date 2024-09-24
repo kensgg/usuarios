44b2827f1f2b9eb82c99aa227e84eee38e05bb48
kensgg <g03.gudicruz@gmail.com>
2024-09-23 22:57:39 -0600

    hi

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const usuariosRutas=require("./routes/rutasUsuarios");
 const app = express();
-const port= process.env.PORT || 4000;
+const port= process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
